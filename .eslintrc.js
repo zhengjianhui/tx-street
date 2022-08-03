d303b01214cc3a2359e7c7c3f0d640d320a9a801
“zhengjianhui” <815168733@qq.com>
2022-08-04 05:12:40 +0800

    chore(doc): update husky

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  plugins: ['prettier', 'import'],
+  ignorePatterns: ['*.js', 'generated'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+    'import/order': ['warn', { alphabetize: { order: 'asc' } }],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+  },
+};
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,26 +1,25 @@
-import Koa from "koa";
-import json from "koa-json";
-import logger from "koa-logger";
-import KoaRouter from "koa-router";
-import parser from "koa-bodyparser";
-import koaStatic from "koa-static";
-import path from "path";
+import path from 'path';
+import * as dotenv from 'dotenv';
+import Koa from 'koa';
+import parser from 'koa-bodyparser';
+import json from 'koa-json';
+import logger from 'koa-logger';
+import KoaRouter from 'koa-router';
+import koaStatic from 'koa-static';
 
-require('dotenv').config({
-    path: path.join(__dirname,'../../../','.env'),
-})
+dotenv.config({
+  path: path.join(__dirname, '../../../', '.env'),
+});
 
-const app:Koa = new Koa();
+const app: Koa = new Koa();
 
 app.use(json());
 app.use(parser());
 app.use(logger());
 
-app.use(koaStatic(
-    path.join( __dirname, '../static')
-))
+app.use(koaStatic(path.join(__dirname, '../static')));
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log(`seccess start server`)
-    console.log(`local: http://127.0.0.1:${process.env.SERVER_PORT}`)
-})
+  console.log(`seccess start server`);
+  console.log(`local: http://127.0.0.1:${process.env.SERVER_PORT}`);
+});
